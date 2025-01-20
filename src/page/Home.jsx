1240772b9a3cfc7becd1a5fedcc460a753bec319
Sabuj-Chowdhury <csabuj73@gmail.com>
2025-01-20 20:03:01 +0530

    trying lazy load to optimise loading time and deploy

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -3,18 +3,31 @@ import Banner from "../components/Banner";
 import Contact from "../components/Contact";
 import Education from "../components/Education";
 import Projects from "../components/Projects";
+import LazyLoad from "react-lazyload";
 
 import Skills from "../components/Skills";
 
 const Home = () => {
   return (
     <section>
-      <Banner></Banner>
-      <About></About>
-      <Skills></Skills>
-      <Education></Education>
-      <Projects></Projects>
-      <Contact></Contact>
+      <LazyLoad height={200} once>
+        <Banner></Banner>
+      </LazyLoad>
+      <LazyLoad height={200} once>
+        <About></About>
+      </LazyLoad>
+      <LazyLoad height={200} once>
+        <Skills></Skills>
+      </LazyLoad>
+      <LazyLoad height={200} once>
+        <Education></Education>
+      </LazyLoad>
+      <LazyLoad height={200} once>
+        <Projects></Projects>
+      </LazyLoad>
+      <LazyLoad height={200} once>
+        <Contact></Contact>
+      </LazyLoad>
     </section>
   );
 };
